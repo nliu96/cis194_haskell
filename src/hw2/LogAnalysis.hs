6ba2ef0390caf34153c9e54f0e7864aa06275627
Nicholas Liu <nickliu96@gmail.com>
2018-05-07 02:24:28 -0400

    Added finished implementation for hw2

--- src/hw2/LogAnalysis.hs
+++ src/hw2/LogAnalysis.hs
@@ -12,3 +12,26 @@ parseMessage x = case (words x) of
   ("W":time:message) -> LogMessage Warning (read time) (unwords message)
   ("E":sev:time:message) -> LogMessage (Error (read sev)) (read time) (unwords message)
   _ -> Unknown x
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown  _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ time _) tree@(Node left nodeMsg@(LogMessage _ nodeTime _) right)
+  | time > nodeTime = Node left nodeMsg (insert msg right)
+  | time < nodeTime = Node (insert msg left) nodeMsg right
+  | otherwise = tree
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build [x] = Node Leaf x Leaf
+build (x:xs) = insert x (build xs)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong ((LogMessage (Error sev) _ msg):logMsgs)
+  | sev > 50 = [msg] ++ (whatWentWrong logMsgs)
+  | otherwise = whatWentWrong logMsgs
