0a95b1e943de31ddc30bb0f634e776fee3b97af2
Nicholas Liu <nickliu96@gmail.com>
2018-05-09 15:52:00 -0400

    Finished hw3 exercise 2

--- src/hw3/Golf.hs
+++ src/hw3/Golf.hs
@@ -1,6 +1,7 @@
 module Golf
 (
-  skips
+  skips,
+  localMaxima
 ) where
 
 skips :: [a] -> [[a]]
@@ -9,3 +10,9 @@ skips xs = zipWith every [1..length xs] (repeat xs)
 every n xs = case drop (n-1) xs of
   (y:ys) -> y : every n ys
   [] -> []
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs)
+  | x < y && y > z = y : localMaxima (y:z:xs)
+  | otherwise = localMaxima (y:z:xs)
+localMaxima _ = []
--- test/hw3/HW03Test.hs
+++ test/hw3/HW03Test.hs
@@ -7,7 +7,7 @@ import Golf
 
 main :: IO ()
 main = do
-  defaultMain (testGroup "HW3 Test" [skipsTest1, skipsTest2, skipsTest3, skipsTest4])
+  defaultMain (testGroup "HW3 Test" [skipsTest1, skipsTest2, skipsTest3, skipsTest4, localMaximaTest1, localMaximaTest2, localMaximaTest3])
 
 skipsTest1 :: TestTree
 skipsTest1 = testCase "Testing skipsTest"
@@ -28,3 +28,16 @@ skipsTest4 = testCase "Testing skipsTest"
 {-skipsTest5 :: TestTree
 skipsTest5 = testCase "Testing skipsTest"
   (assertEqual "skips []" [] (skips []))-}
+
+localMaximaTest1 :: TestTree
+localMaximaTest1 = testCase "Testing localMaxima"
+  (assertEqual "localMaxima [2,9,5,6,1]" [9,6] (localMaxima [2,9,5,6,1]))
+
+localMaximaTest2 :: TestTree
+localMaximaTest2 = testCase "Testing localMaxima"
+  (assertEqual "localMaxima [2,3,4,1,5]" [4] (localMaxima [2,3,4,1,5]))
+
+localMaximaTest3 :: TestTree
+localMaximaTest3 = testCase "Testing localMaxima"
+  (assertEqual "localMaxima [1,2,3,4.5]" [] (localMaxima [1,2,3,4,5]))
+
